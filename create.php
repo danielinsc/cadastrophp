ad1f1265361cd3b49c469e8cd6210802b93132d0
Daniel Lins <daniellins@MacBook-Pro-de-Daniel.local>
2023-11-30 06:13:58 -0300

    first commit

---
+++ create.php
@@ -0,0 +1,133 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "crudphp";
+
+// Cria a conexao com o banco de dados
+$connection = new mysqli($servername, $username, $password, $database);
+
+
+$name = "";
+$email = "";
+$phone = "";
+$address = "";
+
+$errorMessage = "";
+$successMessage = "";
+
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $address = $_POST["address"];
+
+    do {
+        if ( empty($name) || empty($email) || empty($phone) || empty($address) ) {
+            $errorMessage = "Todos os campos são obrigatórios";
+            break;
+        }
+
+        // adiciona novo cliente no banco de dados
+        $sql =  "INSERT INTO clients (name, email, phone, address) " .
+                "VALUES ('$name', '$email', '$phone', '$address')";
+        $result = $connection->query($sql);
+
+        if (!$result) {
+            $errorMessage = "Query Invalida: " . $connection->error;
+            break;
+        }
+
+        $name = "";
+        $email = "";
+        $phone = "";
+        $address = "";
+
+        $successMessage = "Adicione o cliente corretamente";
+
+        header("location: /crudphp/index.php");
+        exit;
+
+    } while (false);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Php</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container my-5">
+        <h2>Novo Cliente</h2>
+
+        <?php
+        if ( !empty($errorMessage) ) {
+            echo "
+            <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                <strong>$errorMessage</strong>
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+            </div>
+            ";
+        }
+        ?>
+
+        <form method="post">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Nome</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Email</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Telefone</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Endereço</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
+                </div>
+            </div>
+
+
+            <?php
+            if ( !empty($successMessage) ) {
+                echo "
+                <div class='row mb-3'>
+                    <div class='offset-sm-3 col-sm-6'>
+                        <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                            <strong>$successMessage</strong>
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                        </div>
+                    </div>
+                </div>
+                ";
+            }
+            ?>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="/myshop/index.php" role="button">Cancelar</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
